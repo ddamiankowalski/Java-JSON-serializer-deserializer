252d218d9ae11aef24e424f61e3ef2f4d4447f77
ddamiankowalski <dkowalski.1997@gmail.com>
2021-12-23 15:23:01 +0100

    added tests

---
+++ src/test/java/com/example/jsonparser/SerachWordsTest.java
@@ -0,0 +1,38 @@
+package com.example.jsonparser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SerachWordsTest extends TestCase {
+    Dictionary dictionary = new Dictionary();
+
+    @Before
+    public void setUp() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        dictionary = mapper.readValue(new File("src/main/java/com/example/jsonparser/English.json"), Dictionary.class);
+    }
+
+    // the test below checks whether the function searchWord retuns the String if the word does exist in the dictionary
+    @Test
+    public void testSearchWords() {
+        Assert.assertEquals(dictionary.searchWord("abortion"), "abortion");
+    }
+
+    // the test below checks whether the function searchWord returns null if the word does not exist in the dictionary
+    @Test
+    public void testSearchWordsNull() {
+        Assert.assertEquals(dictionary.searchWord("foo"), null);
+    }
+
+    // the test below checks whether the function gives back the right language
+    @Test
+    public void testGetLanguage() {
+        Assert.assertEquals(dictionary.getLanguage(), "English");
+    }
+}
